bff9f4a731a59d50e6f0e6129c8d389d07e4e8a7
rkfg <rkfg@rkfg.me>
2018-02-03 01:41:42 +0300

    Cosmetics

--- src/AES.cpp
+++ src/AES.cpp
@@ -18,7 +18,7 @@ vector<unsigned char> AES::encrypt(const std::vector<unsigned char>& data) {
 	auto ctx = EVP_CIPHER_CTX_new();
 	EVP_EncryptInit(ctx, cipher_, (const unsigned char*)key_.c_str(), (const unsigned char*)iv_.c_str());
 	size_t len = data.size();
-	unsigned char out[(len / 16  + 1) * 16];
+	unsigned char out[(len >> 4 + 1) << 4];
 	int outl;
 	EVP_EncryptUpdate(ctx, out, &outl, (const unsigned char*)data.data(), len);
 	vector<unsigned char> result(out, out + outl);
@@ -32,7 +32,7 @@ std::vector<unsigned char> AES::decrypt(const std::vector<unsigned char>& data) 
 	auto ctx = EVP_CIPHER_CTX_new();
 	EVP_DecryptInit(ctx, cipher_, (const unsigned char*)key_.c_str(), (const unsigned char*)iv_.c_str());
 	size_t len = data.size();
-	unsigned char out[(len / 16  + 1) * 16];
+	unsigned char out[(len >> 4 + 1) << 4];
 	int outl;
 	EVP_DecryptUpdate(ctx, out, &outl, (const unsigned char*)data.data(), len);
 	vector<unsigned char> result(out, out + outl);
